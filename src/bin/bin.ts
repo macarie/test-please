ff4b99130d0df4acb862c85653b79e236a8b99a6
Raul Macarie <raul@macarie.me>
2021-07-21 00:23:01 +0200

    Parse and handle base folder and pattern arguments

--- src/bin/bin.ts
+++ src/bin/bin.ts
@@ -1,17 +1,50 @@
 #!/usr/bin/env node
 
 import { cwd } from 'node:process'
+import { resolve as resolvePath } from 'node:path'
 
 import sade from 'sade'
+import { totalist } from 'totalist'
 
 import { exec } from '../runner/runner.js'
 
-sade('test-please [tests]')
+type CLIOptions = {
+  cwd: string
+}
+
+const defaults = {
+  dir: '.',
+  patternWithDir: /\.m?js$/i,
+  patternWithoutDir: /(^(tests?|__tests?__)[\\/].+|[-.](test|spec)?)\.js$/i,
+  ignoredPaths: [/node_modules/i],
+}
+
+sade('test-please [dir] [pattern]')
   .option('-C, --cwd', 'The current directory to resolve from', cwd())
-  .action(async (tests: string | string[], options: { cwd: string }) => {
+  .action(async (dir: string, pattern: string, options: CLIOptions) => {
+    const dirToUse = dir || defaults.dir
+    const patternToUse: RegExp = pattern
+      ? new RegExp(pattern, 'i')
+      : dir
+      ? defaults.patternWithDir
+      : defaults.patternWithoutDir
+
+    const { ignoredPaths } = defaults
+
+    const tests: string[] = []
+
+    await totalist(resolvePath(options.cwd, dirToUse), (relativePath) => {
+      if (
+        !ignoredPaths.every((ignoredPath) => ignoredPath.test(relativePath)) &&
+        patternToUse.test(relativePath)
+      ) {
+        tests.push(resolvePath(dirToUse, relativePath))
+      }
+    })
+
     try {
       await exec({
-        tests: Array.isArray(tests) ? tests : [tests],
+        tests,
         workingDirectory: options.cwd,
       })
     } catch {
