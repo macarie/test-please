6bc7426c4fb165348074ce1531e698b339bdc2c5
Raul Macarie <raul@macarie.me>
2021-07-21 21:40:50 +0200

    Switch from CLI arguments to options

--- src/bin/bin.ts
+++ src/bin/bin.ts
@@ -10,37 +10,90 @@ import { exec } from '../runner/runner.js'
 
 type CLIOptions = {
   cwd: string
+  dir: string | string[]
+  pattern: string | string[]
+  ignore: string | string[]
 }
+
+const toRegExp = (input: string) => new RegExp(input, 'i')
 
 const defaults = {
   dir: '.',
-  patternWithDir: /\.m?js$/i,
-  patternWithoutDir: /(^(tests?|__tests?__)[\\/].+|[-.](test|spec)?)\.js$/i,
+  patternWithDir: [/\.m?js$/i],
+  patternWithoutDir: [/(^(tests?|__tests?__)[\\/].+|[-.](test|spec)?)\.js$/i],
   ignoredPaths: [/node_modules/i],
 }
 
-sade('test-please [dir] [pattern]')
-  .option('-C, --cwd', 'The current directory to resolve from', cwd())
-  .action(async (dir: string, pattern: string, options: CLIOptions) => {
-    const dirToUse = dir || defaults.dir
-    const patternToUse: RegExp = pattern
-      ? new RegExp(pattern, 'i')
-      : dir
-      ? defaults.patternWithDir
-      : defaults.patternWithoutDir
+const getDir = (dir: string | string[] = defaults.dir) => {
+  if (Array.isArray(dir)) {
+    return dir
+  }
 
-    const { ignoredPaths } = defaults
+  return [dir]
+}
+
+const getPattern = (pattern: string | string[], dir?: string | string[]) => {
+  if (pattern === undefined) {
+    if (dir === undefined) {
+      return defaults.patternWithoutDir
+    }
+
+    return defaults.patternWithDir
+  }
+
+  if (Array.isArray(pattern)) {
+    return pattern.map((p) => toRegExp(p))
+  }
+
+  return [toRegExp(pattern)]
+}
+
+const getIgnored = (ignored: string | string[]) => {
+  if (ignored === undefined) {
+    return defaults.ignoredPaths
+  }
+
+  if (Array.isArray(ignored)) {
+    return [...defaults.ignoredPaths, ...ignored.map((i) => toRegExp(i))]
+  }
+
+  return [...defaults.ignoredPaths, toRegExp(ignored)]
+}
+
+sade('test-please', true)
+  .option(
+    '-C, --cwd',
+    'The location from which lookups should be resolved',
+    cwd()
+  )
+  .option('-d, --dir', "The test files' location")
+  .option(
+    '-p, --pattern',
+    "A pattern that the test files' path should match to be executed"
+  )
+  .option(
+    '-i, --ignore',
+    'A pattern used to ignore files, `node_modules` is always ignored'
+  )
+  .action(async (options: CLIOptions) => {
+    const dirs: string[] = getDir(options.dir)
+    const patterns: RegExp[] = getPattern(options.pattern, options.dir)
+    const ignored: RegExp[] = getIgnored(options.ignore)
 
     const tests: string[] = []
 
-    await totalist(resolvePath(options.cwd, dirToUse), (relativePath) => {
-      if (
-        !ignoredPaths.every((ignoredPath) => ignoredPath.test(relativePath)) &&
-        patternToUse.test(relativePath)
-      ) {
-        tests.push(resolvePath(dirToUse, relativePath))
-      }
-    })
+    await Promise.all(
+      dirs.map(async (dir) =>
+        totalist(resolvePath(options.cwd, dir), (relativePath) => {
+          if (
+            !ignored.every((ignoredPath) => ignoredPath.test(relativePath)) &&
+            patterns.some((pattern) => pattern.test(relativePath))
+          ) {
+            tests.push(resolvePath(dir, relativePath))
+          }
+        })
+      )
+    )
 
     try {
       await exec({
