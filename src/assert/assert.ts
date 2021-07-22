dc66a7394927406a02633d6a160c6521067223b1
Raul Macarie <raul@macarie.me>
2021-07-23 00:43:42 +0200

    Add the first two `assert` methods: `is` and `equal`

---
+++ src/assert/assert.ts
@@ -0,0 +1,54 @@
+import type { Assertion } from './types/assertion.js'
+import type { Diff } from './types/diff.js'
+
+import { dequal } from 'dequal'
+
+import { AssertionError } from './helpers/assertion-error.js'
+import { compare } from './helpers/compare.js'
+
+const assert = (
+  satisfied: boolean,
+  assertion: Assertion,
+  diff: Diff[][],
+  message: string | undefined,
+  stackTraceEnd: Function // eslint-disable-line @typescript-eslint/ban-types
+) => {
+  if (satisfied) {
+    return
+  }
+
+  throw new AssertionError({
+    assertion,
+    diff,
+    message,
+    stackTraceEnd,
+  })
+}
+
+export const is = <AssertionType>(
+  actual: AssertionType,
+  expected: AssertionType,
+  message?: string
+) => {
+  assert(
+    Object.is(actual, expected),
+    'is',
+    compare(actual, expected),
+    message,
+    is
+  )
+}
+
+export const equal = <AssertionType>(
+  actual: AssertionType,
+  expected: AssertionType,
+  message?: string
+) => {
+  assert(
+    Object.is(actual, expected),
+    'equal',
+    compare(actual, expected),
+    message,
+    equal
+  )
+}
